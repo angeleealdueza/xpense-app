678e7ea5add926a659f5beb82fa5eb1a618f7de0
angeleealdueza <angeleealdueza@gmail.com>
2022-09-23 00:36:00 +0800

    add Models

---
+++ Xpense App/Xpense App/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Xpense_App.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }  //Navigational property
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date{ get; set; } = DateTime.Now;
+
+
+    }
+}
