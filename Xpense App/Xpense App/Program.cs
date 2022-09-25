bc4d6ecc731e321836994154779723b28ebce4aa
angeleealdueza <angeleealdueza@gmail.com>
2022-09-25 22:24:14 +0800

    register syncfusion

--- Xpense App/Xpense App/Program.cs
+++ Xpense App/Xpense App/Program.cs
@@ -10,6 +10,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
+//Register Syncfusion license
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2VVhiQlFaclxJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdkxhXH5ecXxWT2JYVkI=");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
