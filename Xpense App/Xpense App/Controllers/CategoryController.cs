fb768951f2dbe79c9854b53c498c719fa7b04d18
angeleealdueza <angeleealdueza@gmail.com>
2022-09-28 00:26:52 +0800

    update design form

--- Xpense App/Xpense App/Controllers/CategoryController.cs
+++ Xpense App/Xpense App/Controllers/CategoryController.cs
@@ -61,7 +61,11 @@ namespace Xpense_App.Controllers
         {
             if (ModelState.IsValid)
             {
-                _context.Add(category);
+                if (category.CategoryId == 0)
+                    _context.Add(category);
+                else
+                    _context.Update(category);
+
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
