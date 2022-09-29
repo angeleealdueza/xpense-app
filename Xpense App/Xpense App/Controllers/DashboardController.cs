103585a53151e29f3f2fa8e2c5d95144cc799fd6
angeleealdueza <angeleealdueza@gmail.com>
2022-09-29 14:49:46 +0800

    add Dashboard Controller and View

---
+++ Xpense App/Xpense App/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xpense_App.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
