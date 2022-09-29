83f31ca92c1bcadc252e955b048c7e4f6e1aa3eb
angeleealdueza <angeleealdueza@gmail.com>
2022-09-30 01:26:30 +0800

    add code for balance

--- Xpense App/Xpense App/Controllers/DashboardController.cs
+++ Xpense App/Xpense App/Controllers/DashboardController.cs
@@ -36,6 +36,10 @@ namespace Xpense_App.Controllers
                 .Sum(j => j.Amount);
             ViewBag.TotalExpense = TotalExpense.ToString("C0");
 
+            //Balance
+            int Balance = TotalIncome - TotalExpense;
+            ViewBag.Balance = Balance.ToString("C0");
+
             return View();
         }
     }
