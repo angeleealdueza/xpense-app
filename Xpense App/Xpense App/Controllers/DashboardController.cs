711efaf21d2d5c4c1a8b6289c53fae913fa3c17a
angeleealdueza <angeleealdueza@gmail.com>
2022-09-30 01:23:43 +0800

    update dashboard

--- Xpense App/Xpense App/Controllers/DashboardController.cs
+++ Xpense App/Xpense App/Controllers/DashboardController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xpense_App.Models;
 
 namespace Xpense_App.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
-        {
+        private readonly ApplicationDbContext _context;
 
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //Last 7 Days
+            DateTime StartDate  =  DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date >= StartDate && y.Date <= EndDate)
+                .ToListAsync();
+
+            //Total Income
+            int TotalIncome = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(j => j.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
 
             return View();
         }
